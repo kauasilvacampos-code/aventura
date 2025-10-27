7e450fbc4e20774cf47190eae9215d8ef80581e0
kauasilvacampos-code <kaua.silva.campos@escola.pr.gov.br>
2025-10-27 09:42:33 -0300

    Create kauaaula2.js

---
+++ kauaaula2.js
@@ -0,0 +1,9 @@
+constavanca=docunent.querySelectorAll(.btn-proximo)
+avancaForEach(button {
+   buttonaddEventListener('click',function(){
+     constatual=document.querySelector('ativo');
+     constproximoPasso='passo-' tthis.getAttrbute('data-proximo');
+     atual-classList.remove('ativo')
+     document.getElementByTd(proximopasso).classList.add('ativo');
+   })
+})
